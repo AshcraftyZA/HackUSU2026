15234b36c78397391ca00a7ed07b73acf56ad633
AshcraftyZA <ashcraftza@gmail.com>
2026-02-28 11:45:16 +0000

    Co-authored-by: Rachael-Gentry <Rachael-Gentry@users.noreply.github.com>

--- code.py
+++ code.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import statsmodels.api as sm
+from statsmodels.stats.anova import anova_lm
 
-test =pd.read_csv("/workspaces/HackUSU2026/GitHubTest.csv")
+MaxDif = pd.read_csv("/workspaces/HackUSU2026/DataResults1.csv")
 
-print(test.head())
+# print(MaxDif.head())
+
+# print(MaxDif.info())
+# print(MaxDif.describe())
+print(MaxDif['Milkshake Mixes'].head())
+
